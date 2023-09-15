be4dc4447472fdcecd180543f503695ab5bdc85b
Taweechai Yuenyang <krumii.it@gmail.com>
2023-09-15 15:29:31 +0700

    js create custom button

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,14 @@
-// Copyright (c) 2023, Taweechai Yuenyang and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Library Member', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        frm.add_custom_button('Create Membership', () => {
+            frappe.new_doc('Library Membership', {
+                library_member: frm.doc.name
+            })
+        })
+        frm.add_custom_button('Create Transaction', () => {
+            frappe.new_doc('Library Transaction', {
+                library_member: frm.doc.name
+            })
+        })
+    }
 });
